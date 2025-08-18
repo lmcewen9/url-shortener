f1d88e56f374b4c06ad33b4b1feb0f13a2341593
lmcewen9 <89426396+lmcewen9@users.noreply.github.com>
2025-08-18 19:12:55 -0400

    inital logic

---
+++ shorten/url-short.go
@@ -0,0 +1,75 @@
+package shorten
+
+import (
+	"fmt"
+	"math/rand/v2"
+	"net/http"
+	"time"
+)
+
+const URL = "http://localhost:8080/"
+
+type URLShortener struct {
+	urls map[string]string
+}
+
+func GenerateShortKey() string {
+	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	const keylength = 3
+
+	seed := time.Now().UnixNano()
+	source := rand.NewPCG(uint64(seed), rand.New(rand.NewPCG(uint64(seed), uint64(seed+1))).Uint64())
+	r := rand.New(source)
+
+	shortKey := make([]byte, keylength)
+	for i := range shortKey {
+		shortKey[i] = charset[r.IntN(len(charset))]
+	}
+	return string(shortKey)
+}
+
+func (us *URLShortener) HandleShorten(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Invalid Request Method...", http.StatusMethodNotAllowed)
+		return
+	}
+
+	ogUrl := r.FormValue("url")
+	if ogUrl == "" {
+		http.Error(w, "URL paramerter is missing...", http.StatusBadRequest)
+		return
+	}
+
+	shortKey := GenerateShortKey()
+	us.urls[shortKey] = ogUrl
+
+	shortenedURL := fmt.Sprintf("%s%s", URL, shortKey)
+
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	responseHTML := fmt.Sprintf(`
+		<h2>URL Shortener</h2>
+        <p>Original URL: %s</p>
+        <p>Shortened URL: <a href="%s">%s</a></p>
+        <form method="post" action="/shorten">
+            <input type="text" name="url" placeholder="Enter a URL">
+            <input type="submit" value="Shorten">
+        </form>
+	`, ogUrl, shortenedURL, shortenedURL)
+	fmt.Fprint(w, responseHTML)
+}
+
+func (us *URLShortener) HandleRedirect(w http.ResponseWriter, r *http.Request) {
+	shortKey := r.URL.Path[len(URL):]
+	if shortKey == "" {
+		http.Error(w, "Shortened key is missing...", http.StatusBadRequest)
+		return
+	}
+
+	ogURL, found := us.urls[shortKey]
+	if !found {
+		http.Error(w, "Shortened key not found...", http.StatusNotFound)
+		return
+	}
+
+	http.Redirect(w, r, ogURL, http.StatusMovedPermanently)
+}
